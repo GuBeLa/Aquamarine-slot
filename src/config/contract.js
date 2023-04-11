701895ac6cc558f648763f903e3cd70f47389db6
Irakli Gubelidze <igubelidze@ip.local>
2023-04-11 17:48:48 +0400

    add project structure

---
+++ src/config/contract.js
@@ -0,0 +1,18 @@
+export const ConfigInterface = {
+    gameWidth: '',
+    gameHeight: '',
+    reelsPosition: { x: '', y: '' },
+    playButtonPosition: { x: '', y: '' },
+    selectEasyModePosition: { x: '', y: '' },
+    FPSDisplayPosition: { x: '', y: '' },
+    totalReels: '',
+    reelSpinningCycles: '',
+    reelSpinningSpeedFactor: [],
+    reelShuffleSpinningSpeedFactor: false,
+    totalReelCells: '',
+    reelCellHeight: '',
+    reelCellWidth: '',
+    reelVisibleCells: '',
+    reelVerticalPadding: '',
+    reelHorizontalMargin: '',
+  }
